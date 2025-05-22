4c031cfbe0f31a6a106118e3b1a31907429ac754
Goodvibes74 <kazibwed499@gmail.com>
2025-05-22 13:13:16 +0300

    Refactor main.dart and widget_test.dart for improved readability and consistency

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  runApp(MyApp());
+  }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  //Variable
+  String name="Joshua";
+  int age=20;
+  double pi=3.14159;
+  bool isNew=true;
+  bool isOld=false;
+  int a=10;
+  int b=20;
   @override
   Widget build(BuildContext context) {
+  /*  Programming fundamentals  dart
+    //basic math operations(+, -, *, /, %-modulus)
+      print (a+b);
+      print(a - b);
+      print(a * b);
+      print  (a % b);
+      print(a / b);
+    //Comparison operators(>, <, >=, <=, ==, !=-not equal, ===-strict equal, !==-strict not equal)
+      print (a==b);
+      print(a != b);
+      print(a > b);
+      print(a < b);
+      print(a >= b);
+      print(a <= b);
+    //Assignment operators(=-equal, +=-add and assign, -=-subtract and assign, *=-multiply and assign, /=-divide and assign, %=modulus and assign)
+      print(a = b);
+      print(a += b);
+      print(a -= b);
+      print(a *= b);
+      print(a %= b);
+      a+=10;
+      print(a);
+    //Increment and Decrement operators(++-increment, ---decrement)
+      print(a++);
+      print(a--);
+      print(++a);
+      print(--a);
+    //Logical operators(&&-and, ||-or, !-not, ^-xor, ~-not, &-and, |-or)
+      print(isNew && isOld);
+      print(isNew || isOld);
+      print(!isNew);
+      print(!isOld);
+      print(isNew ^ isOld);
+      print(isNew & isOld);
+      print(isNew | isOld);
+  */
+
+    /* CONTROL FLOW
+    1. if (condtion){
+        do something
+      }
+      else if(condition){
+        do something else
+      }
+      else{
+        do something else at the end of the if statement
+      }
+    */
+    if (name =="Joshua" && age>= 18){
+      print("You are an adult");
+    }
+    else if (name =="Joshua" && age< 18){
+      print("You are a minor");
+    }
+    else{
+      print("You are not Joshua");
+    }
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    ); // returns a MaterialApp widget (dark screen initially)
   }
 }
