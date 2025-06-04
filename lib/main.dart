68849707ce0069619880e78a8d11e76fb2ca3d58
Goodvibes74 <kazibwed499@gmail.com>
2025-06-04 13:05:04 +0300

    working in columns and rows - Stacks

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,33 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueAccent[50],
         body:
-        GridView.builder(
-          itemCount: 64,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-          itemBuilder: (context, index) => Container(
-            color: Colors.blue,
-            margin: EdgeInsets.all(2),
-          )  ,
-        ),
+        Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              //big box
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.blue,
+              ),
+
+              //medium box
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.blue[400],
+              ),
+
+              //tiny box
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue[200],
+              )
+            ],
+          ),
+        )
       ),
     );
   }
