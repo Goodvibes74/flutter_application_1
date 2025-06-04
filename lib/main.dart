a1bcfdffb5a2c327d451407639a51aa533fd5778
Goodvibes74 <kazibwed499@gmail.com>
2025-06-04 13:47:24 +0300

    working gestures

--- lib/main.dart
+++ lib/main.dart
@@ -10,41 +10,32 @@ class MyApp extends StatelessWidget {
    MyApp({super.key});
 
   List names= ["Jake","Mary", "Joseph"];
-
+void usertapped(){
+  print("Tapped container!");
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueAccent[50],
-        body:
-        Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              //big box
-              Container(
-                height: 300,
-                width: 300,
-                color: Colors.blue,
+        body: Center(
+          child: GestureDetector(
+            onTap: usertapped,
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue[300],
+              child: Center(
+                  child:
+                  Text(
+                    "Tap me",
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+                  )
               ),
-
-              //medium box
-              Container(
-                height: 200,
-                width: 200,
-                color: Colors.blue[400],
-              ),
-
-              //tiny box
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue[200],
-              )
-            ],
+            ),
           ),
-        )
+        ),
       ),
     );
   }
