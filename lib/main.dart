5feee07ffe38ddbabf115ed324cda421f94d75e8
Goodvibes74 <kazibwed499@gmail.com>
2025-06-04 12:46:07 +0300

    working in columns and rows - ListView builder using existing data structures

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
+
+  List names= ["Jake","Mary", "Joseph"];
 
   @override
   Widget build(BuildContext context) {
@@ -13,77 +15,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueAccent[50],
-        /*appBar: AppBar(
-          title:
-          Text("My App Bar",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-            ),
-          ),
-          backgroundColor: Colors.blueAccent,
-          elevation: 0,
-            leading: Icon(Icons.menu, color: Colors.white,),
-            actions: [
-              IconButton(
-                  onPressed: (){},
-                  icon: Icon(Icons.logout, color: Colors.white,)
-              )
-            ],
-        ),*/
         body:
-        /* Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              // curved corners
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: EdgeInsets.all(30),
-            child: /*Text(
-                "David Rules",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-            ),*/
-            Icon(
-              Icons.favorite,
-              color: Colors.white,
-              size: 64,
-            )
+        ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(names[index]),
           ),
-        )*/
-        Column(
-          /*crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,*/
-          children: [
-            //box_1
-            Expanded(
-              child: Container(
-                  color: Colors.blue,
-              ),
-            ),
-
-            //box_2
-            Expanded(
-              child: Container(
-                  color: Colors.blue[400]
-              ),
-            ),
-
-            //box_3
-            Expanded(
-              child: Container(
-                  color: Colors.blue[200]
-              ),
-            ),
-          ],
         ),
-      ),
+      )
     );
   }
 }
